139e33036ef07e474030976dc73037616278a65d
narmina5 <narminmailova5@gmail.com>
2026-05-17 22:41:58 +0400

    added ders23

---
+++ ders23/main.py
@@ -0,0 +1,31 @@
+"""
+Git - GitHub - GitLab - BitBucket
+Terminler:
+  Repository - repo - Proyekt
+  Branch - budaq
+  Clone
+  Pull - chekmek
+  Push - yuklemek (GitHuba)
+  Commit - deyishiklikler
+  Pull Request
+  Merge
+  Conflict
+
+Lazimli git commandlari:
+  git clone
+  git add <filename>
+  git commit -m"Message"
+  git push
+  git pull
+  git log
+  git checkout <branch>
+  git status
+  git merge <branch>
+
+  git checkout
+
+  git reset --hard <commit_id>  (meselen git reset --hard 8ae01e0d9cefd1ebcbcc1fb2172b7c8785b7492d)
+
+.gitignore
+README.md
+"""
